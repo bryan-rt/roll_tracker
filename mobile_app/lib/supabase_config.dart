4991a84d8be43a4d3d9a8899a9da37f35be699b8
Bryan Thomas <bryanrt.bt@gmail.com>
2026-03-15 19:34:25 -0400

    Use local Supabase URL and key
    Update mobile_app/lib/supabase_config.dart to point to a local Supabase instance (http://192.168.0.66:54321) and replace the previous cloud URL and key with a local publishable key for development/testing purposes.

--- mobile_app/lib/supabase_config.dart
+++ mobile_app/lib/supabase_config.dart
@@ -1,6 +1,6 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-const String supabaseUrl = 'https://ugqfesjfyfacivpneazq.supabase.co';
-const String supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVncWZlc2pmeWZhY2l2cG5lYXpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxMTA1OTUsImV4cCI6MjA2MzY4NjU5NX0.e8iBU_XDhJ0jOfUmHV64jMO-QcgV9e0QrcDxXwweqHo';
+const String supabaseUrl = 'http://192.168.0.66:54321';
+const String supabaseKey = 'sb_publishable_ACJWlzQHlZjBrEguHvfOxg_3BJgxAaH';
 
 SupabaseClient get supabase => Supabase.instance.client;
